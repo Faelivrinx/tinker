ef3558b2c628e86b743545a0e62f0188cef68aa3
Faelivrinx <jurasz.do@gmail.com>
2025-01-22 21:43:37 +0100

    added basic security

---
+++ src/main/kotlin/com/dominikdev/tinker/HealthEndpoint.kt
@@ -0,0 +1,13 @@
+package com.dominikdev.tinker
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+//@RestController
+//class HealthEndpoint {
+//
+//    @GetMapping(value = ["/test"])
+//    fun health(): String {
+//        return "OK"
+//    }
+//}
